ad32ba1a633db7c67c1764349bb21e16b8a4fc98
83229396@qq.com <guoyaya521>
2019-05-23 22:17:06 +0800

    init

--- hellorabbitmq/src/test/java/com/li/HellorabbitmqApplicationTests.java
+++ hellorabbitmq/src/test/java/com/li/HellorabbitmqApplicationTests.java
@@ -2,15 +2,38 @@ package com.li;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = HellorabbitmqApplication.class)
 public class HellorabbitmqApplicationTests {
 
+    @Autowired
+    private AmqpTemplate rabbitmqTemplate;
+    /**
+     * 测试rqbbitmq链接生产数据
+     */
     @Test
-    public void contextLoads() {
+    public void testProvider() {
+
+        int i =0;
+        while (true) {
+            //连接名为hello的消息队列
+            rabbitmqTemplate.convertAndSend("hello",i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("生产数据：" + i);
+            i++;
+
+        }
+
+
     }
 
 }
