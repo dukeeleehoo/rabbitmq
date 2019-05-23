9dbe159cbbfc2d5039a0ffa16eb1d6d260dd31c9
83229396@qq.com <guoyaya521>
2019-05-23 21:40:29 +0800

    init file

---
+++ hellorabbitmq/src/main/java/com/li/HellorabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.li;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellorabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellorabbitmqApplication.class, args);
+    }
+
+}
